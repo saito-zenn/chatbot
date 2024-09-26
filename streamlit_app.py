3d2edd6868e915a8d7c204dd84a0f17a9019dc77
saito-zenn <7kasugano@gmail.com>
2024-09-26 02:29:21 +0000

    set apikey

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ import streamlit as st
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("API Key", type="password")
+api_key = st.secrets["OPENAI_API_KEY"]
 if not openai_api_key:
     st.info("Please add your API key to continue.", icon="🗝️")
 else:
